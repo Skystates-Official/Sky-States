304a235a1e0c3ade58f19386d2710f6db782852e
ayushkumarcsit2022 <ayush.kumar.csit.2022@miet.ac.in>
2026-07-05 01:48:12 +0530

    UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
+  server: {
+    host: true,
+    allowedHosts: ['192.168.29.64']
+  },
   integrations: [tailwind({
     applyBaseStyles: false,
   })],
